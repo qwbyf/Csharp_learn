98b0c7cad06980ba5b4e9b6b9d12c023549ebbcd
祈我不与风 <zdb197302@163.com>
2025-10-21 18:29:22 +0800

    Add files via upload

---
+++ C#_基础/C#.3_Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C_._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char A = '花', B = '火';
+            Console.WriteLine(A + "\n" + B);
+        }
+    }
+}
