70f3990c70ae57d28bb0f0c4aa2719e5430e8d92
祈我不与风 <zdb197302@163.com>
2025-10-23 13:43:19 +0800

    Add files via upload

---
+++ C#_基础/C#.5_异常捕获.cs
@@ -0,0 +1,29 @@
+﻿namespace C_._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //异常捕获
+
+            try//希望进行异常捕获的代码
+            {
+                Console.WriteLine("请输入用户名:");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+                Console.WriteLine(i);
+
+            }
+            catch//异常后执行的代码
+            {
+                Console.WriteLine("用户名错误,请输入数字" + "\n请重新输入:");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+                Console.WriteLine(i);
+
+            }
+            finally//无论是否异常最终都执行的代码 
+            { Console.WriteLine("输入完毕"); }
+        }
+    }
+}
