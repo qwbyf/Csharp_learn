3a08d43ca3bfcde82952dd6c9c0cf650c74b4b45
祈我不与风 <zdb197302@163.com>
2025-11-02 22:24:55 +0800

    Add files via upload

---
+++ C#_基础/C#.14_dowhile循环.cs
@@ -0,0 +1,35 @@
+﻿namespace C_._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region do while 循环  //先斩后奏: 先至少执行一次,再判断是否继续循环
+            int i1 = 1; 
+            do
+            {                
+                Console.WriteLine(i1);//1,2
+                i1++;
+            } while (i1 < 3);
+
+
+            //练习
+            string name, key;
+            bool judge = false;
+            do
+            {
+                if (judge)
+                {
+                    Console.WriteLine("用户名或密码错误,请重新输入!");
+                }
+                judge = true;
+                Console.WriteLine("请输入用户名:");
+                name = Console.ReadLine();
+                Console.WriteLine("请输入密码:");
+                key = Console.ReadLine();
+
+            } while (name != "admin" && key != "8888");
+            #endregion
+        }
+    }
+}
